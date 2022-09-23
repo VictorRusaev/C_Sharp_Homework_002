0ffa481a98a5f75595f7505d2496c454164c6e9b
Victor Rusaev <rusaev74@mail.ru>
2022-09-23 16:08:21 +0300

    TASK001_second_from_three

---
+++ TASK001/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+if((number > 100) && (number < 1000))
+{
+    int firstNumber = number % 10;
+    int secondNumber = number / 10 % 10;
+    int lastNumber = number / 100;
+    Console.WriteLine(secondNumber);
+}
+else Console.Write("Вы ввели не трёхзначное число!");
