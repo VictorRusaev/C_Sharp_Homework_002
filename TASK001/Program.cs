bfaa8b12e4a7a22dd7e51ebd7148208c86e4fa68
Victor Rusaev <rusaev74@mail.ru>
2022-09-23 17:25:53 +0300

    Deleted first and second number counting

--- TASK001/Program.cs
+++ TASK001/Program.cs
@@ -4,10 +4,5 @@ Console.Write("Введите трёхзначное число: ");
 int number = int.Parse(Console.ReadLine());
 
 if((number > 99) && (number < 1000))
-{
-    int firstNumber = number % 10;
-    int secondNumber = number / 10 % 10;
-    int lastNumber = number / 100;
-    Console.WriteLine(secondNumber);
-}
+    Console.WriteLine(number / 10 % 10);
 else Console.Write("Вы ввели не трёхзначное число!");
