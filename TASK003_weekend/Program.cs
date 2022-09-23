971dfc0ae1a703121f0c6645a06045a9a89559a1
Victor Rusaev <rusaev74@mail.ru>
2022-09-23 18:11:09 +0300

    TASK003 weekend

---
+++ TASK003_weekend/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число от 1 до 7");
+int weekday = int.Parse(Console.ReadLine());
+
+if((weekday < 1) ^ (weekday > 7))
+    Console.WriteLine("Вы ввели число не от 1 до 7");
+else if((weekday == 6) ^ (weekday == 7))
+    Console.Write("выходной");
+else Console.Write("будний");
